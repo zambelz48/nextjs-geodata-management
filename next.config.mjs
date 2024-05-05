a51a2a242e9a9dce4e537a26d5ee0ca3c72fba89
zambelz48 <nanda.julianda48@gmail.com>
2024-05-05 19:38:13 +0700

    fix: add redirection to geodata list

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/geodata",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
